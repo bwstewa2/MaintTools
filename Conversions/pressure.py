7fde545efb2ede6afd51a79276daa0257d63429c
Bert Stewart <Bert.Stewart@wdc.com>
2026-07-17 12:05:42 -0700

    moves all pressure logic and units to pressure.py file to clean up UnitConversion.py

--- Conversions/pressure.py
+++ Conversions/pressure.py
@@ -1,4 +1,6 @@
- #Pressure Caluculations
+pressure_units = ["Torr", "mTorr", "Bar", "mBar", "Pa", "mPa", "inHg", "psi", "atm"] 
+
+#Pressure Caluculations
 def toMilli(input):
     return input * 1000
 
@@ -101,3 +103,88 @@ def atmToInhg(input):
 
 def atmToPsi(input):
     return input * 14.696
+
+def convert_pressure(input_units, output_units, input):
+    try:
+        input = float(input)
+    except ValueError:
+        return 0
+    
+    if input_units[0] == 'm' and output_units[0] != 'm':
+        input = toNom(input)
+        input_units = input_units[1:]
+    elif input_units[0] != 'm' and output_units[0] == 'm':
+        input = toMilli(input)
+        out_type = out_type[1:]
+    elif input_units[0] == 'm' and output_units[0] == 'm':
+        input_units = input_units[1:]
+        output_units = output_units[1:]
+
+    if input_units == "Torr":
+        if output_units == "Bar":
+            return torrToBar(input)
+        elif output_units == "Pa":
+            return torrToPa(input)
+        elif output_units == "inHg":
+            return torrToInhg(input)
+        elif output_units == "psi":
+            return torrToPsi(input)
+        elif output_units == "atm":
+            return torrToAtm(input)
+    elif input_units == "Bar":
+        if output_units == "Torr":
+            return barToTorr(input)
+        elif output_units == "Pa":
+            return barToPa(input)
+        elif output_units == "inHg":
+            return barToInhg(input)
+        elif output_units == "psi":
+            return barToPsi(input)
+        elif output_units == "atm":
+            return barToAtm(input)
+    elif input_units == "Pa":
+        if output_units == "Torr":
+            return paToTorr(input)
+        elif output_units == "Bar":
+            return paToBar(input)
+        elif output_units == "inHg":
+            return paToInhg(input)
+        elif output_units == "psi":
+            return paToPsi(input)
+        elif output_units == "atm":
+            return paToAtm(input)
+    elif input_units == "inHg":
+        if output_units == "Torr":
+            return inhgToTorr(input)
+        elif output_units == "Bar":
+            return inhgToBar(input)
+        elif output_units == "Pa":
+            return inhgToPa(input)
+        elif output_units == "psi":
+            return inhgToPsi(input)
+        elif output_units == "atm":
+            return inhgToAtm(input)
+    elif input_units == "psi":
+        if output_units == "Torr":
+            return psiToTorr(input)
+        elif output_units == "Bar":
+            return psiToBar(input)
+        elif output_units == "Pa":
+            return psiToPa(input)
+        elif output_units == "inHg":
+            return psiToInhg(input)
+        elif output_units == "atm":
+            return psiToAtm(input)
+    elif input_units == "atm":
+        if output_units == "Torr":
+            return atmToTorr(input)
+        elif output_units == "Bar":
+            return atmToBar(input)
+        elif output_units == "Pa":
+            return atmToPa(input)
+        elif output_units == "inHg":
+            return atmToInhg(input)
+        elif output_units == "psi":
+            return atmToPsi(input)
+    else:
+        return input
